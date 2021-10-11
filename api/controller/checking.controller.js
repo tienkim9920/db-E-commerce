7d95653d4cd4f7ba1b2581b7d3d9a5a775abab21
toannguyen197 <hahadaubo@gmail.com>
2021-10-11 16:11:20 +0700

    1st_Update

--- api/controller/checking.controller.js
+++ api/controller/checking.controller.js
@@ -1,12 +1,57 @@
-// const Checking = require('../model/checking.model')
+const express = require('express')
 
-// const express = require('express')
+const router = express.Router()
 
-// const router = express.Router()
+const Checking = require('../model/checking.model')
 
-// // GET checking all
-// router.get('/', async (req, res) => {
-//     res.json('list checking')
-// })
+// GET Checking all
+router.get('/', async (req, res) => {
+    
+    const checking = await Checking.find({});
+    
+    res.json(checking)
 
-// module.exports = router
+})
+
+// POST Checking
+router.post('/', async (req, res) => {
+    
+    Checking.create(req.body, function(err, result){
+        if(err) {
+            res.json({
+                msg:"0",
+                err
+            })
+        }else{
+            res.json({
+                msg:"1",
+                result
+            })
+        }
+    });
+
+})
+
+// DELETE Checking
+router.delete('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    Checking.deleteOne({ _id: id }, function(err, result) {
+        if (err) {
+            res.json({
+                msg: "That bai",
+                err
+            })
+        } else {
+            res.json({
+                msg: "Thanh Cong",
+                result
+            })
+        }
+    })
+
+})
+
+
+module.exports = router
--- api/controller/client.controller.js
+++ api/controller/client.controller.js
@@ -1,12 +1,57 @@
-// const Client = require('../model/client.model')
+const express = require('express')
 
-// const express = require('express')
+const router = express.Router()
 
-// const router = express.Router()
+const Client = require('../model/client.model')
 
-// // GET client all
-// router.get('/', async (req, res) => {
-//     res.json('list client')
-// })
+// GET Client all
+router.get('/', async (req, res) => {
+    
+    const client = await Client.find({});
+    
+    res.json(client)
 
-// module.exports = router
+})
+
+// POST Client
+router.post('/', async (req, res) => {
+    
+    Client.create(req.body, function(err, result){
+        if(err) {
+            res.json({
+                msg:"0",
+                err
+            })
+        }else{
+            res.json({
+                msg:"1",
+                result
+            })
+        }
+    });
+
+})
+
+// DELETE Client
+router.delete('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    Client.deleteOne({ _id: id }, function(err, result) {
+        if (err) {
+            res.json({
+                msg: "That bai",
+                err
+            })
+        } else {
+            res.json({
+                msg: "Thanh Cong",
+                result
+            })
+        }
+    })
+
+})
+
+
+module.exports = router
